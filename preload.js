d3d194c9000369fd6627e7cffb701a3ebb250214
Badrul1234 <hisyamb896@gmail.com>
2025-11-10 16:07:20 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  requestExport: () => ipcRenderer.send('request:export'),
-  onRequestExport: (cb) => ipcRenderer.on('request:export', cb),
-  sendExportReply: (data) => ipcRenderer.send('reply:export', data)
-});
